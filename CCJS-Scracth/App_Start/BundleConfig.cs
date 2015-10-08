7f846a585f6d16d934d57270db2dac0ba5fc9735
Leo <lsegars@gmail.com>
2015-10-07 21:33:32 -0300

    Corección WebConfig

---
+++ CCJS-Scracth/App_Start/BundleConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace CodeCamper
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+            AddDefaultIgnorePatterns(bundles.IgnoreList);
+
+            bundles.Add(
+                new ScriptBundle("~/scripts/modernizr")
+                    .Include("~/scripts/modernizr-{version}.js"));
+
+            bundles.Add(
+              new ScriptBundle("~/scripts/vendor")
+                .Include("~/scripts/jquery-{version}.min.js")
+                .Include("~/scripts/bootstrap.min.js")
+              );
+
+            bundles.Add(
+             new StyleBundle("~/Content/css")
+                .Include("~/Content/ie10mobile.css") // Must be first. IE10 mobile viewport fix
+                .Include("~/Content/bootstrap.min.css")
+                .Include("~/Content/bootstrap-responsive.min.css")
+                .Include("~/Content/font-awesome.min.css")
+                .Include("~/Content/styles.css")
+             );
+        }
+
+        public static void AddDefaultIgnorePatterns(IgnoreList ignoreList)
+        {
+            if (ignoreList == null)
+            {
+                throw new ArgumentNullException("ignoreList");
+            }
+
+            ignoreList.Ignore("*.intellisense.js");
+            ignoreList.Ignore("*-vsdoc.js");
+            //ignoreList.Ignore("*.debug.js", OptimizationMode.WhenEnabled);
+            //ignoreList.Ignore("*.min.js", OptimizationMode.WhenDisabled);
+            //ignoreList.Ignore("*.min.css", OptimizationMode.WhenDisabled);
+        }
+    }
+}
